0f278b5ab5cd5a4379c4c44aa2722a9f6eff1f56
kalush666 <jonathan.kalush@gmail.com>
2025-07-03 17:02:11 +0300

    controler

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, UpdateUserDto, UserResponseDto } from './dto/user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() dto: CreateUserDto): Promise<UserResponseDto> {
+    return await this.userService.create(dto);
+  }
+
+  @Get()
+  async findAll(): Promise<UserResponseDto[]> {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<UserResponseDto> {
+    return await this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
+    return await this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return await this.userService.remove(id);
+  }
+}
